bdfbec8c00192fc5a7e37bb3ceec6917c716f61d
wjcloud <shnano1128@naver.com>
2017-11-20 19:25:08 +0900

    Scale SIGOPS_COST to 160000

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -14,7 +14,7 @@ static const unsigned int MAX_BLOCK_SERIALIZED_SIZE = 8000000;
 /** The maximum allowed weight for a block, see BIP 141 (network rule) */
 static const unsigned int MAX_BLOCK_WEIGHT = 8000000;
 /** The maximum allowed number of signature check operations in a block (network rule) */
-static const int64_t MAX_BLOCK_SIGOPS_COST = 80000;
+static const int64_t MAX_BLOCK_SIGOPS_COST = 160000;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 100;
 
