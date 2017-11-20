55a56bfd26b3bc7e27d39727c20fd436e600f800
wjcloud <shnano1128@naver.com>
2017-11-20 10:51:14 +0900

    Add pgp keys for gitian build

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -232,8 +232,7 @@ echo ${COMMIT}
 if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
-    git clone https://github.com/bitcoin-core/gitian.sigs.git
-    git clone https://github.com/bitcoin-core/bitcoin-detached-sigs.git
+    git clone https://github.com/BitcoinPlatinum/gitian.sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
